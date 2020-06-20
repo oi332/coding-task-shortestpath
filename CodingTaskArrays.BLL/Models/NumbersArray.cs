0a3c11e5f3a2ecec442c3d7d29a586443f4ffe84
Dovydas <dovydas.tomasiunas@gmail.com>
2020-06-20 18:01:58 +0200

    Add ArrayNumbers validation and tests

---
+++ CodingTaskArrays.BLL/Models/NumbersArray.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTaskArrays.BLL.Models
+{
+    public class NumbersArray
+    {
+        public List<int> Numbers { get; set; }
+    }
+}
---
+++ CodingTaskArrays.BLL/Validators/Models/Graph.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTaskArrays.BLL.Validators.Models
+{
+    public class Graph
+    {
+        public Dictionary<int, List<int>> Relations { get; set; }
+    }
+}
