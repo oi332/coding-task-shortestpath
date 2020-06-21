b8441373b15d58f50bd71618c9e80309d48ae50c
Dovydas <dovydas.tomasiunas@gmail.com>
2020-06-21 16:58:27 +0200

    Add ShortestPathFinder tests

--- CodingTaskArrays.Tests/ShortestPathFinderTests.cs
+++ CodingTaskArrays.Tests/ShortestPathFinderTests.cs
@@ -10,7 +10,7 @@ namespace CodingTaskArrays.Tests
     public class ShortestPathFinderTests
     {
         [Fact]
-        public void IsValid_ArrayNumbersListIsEmpty_False()
+        public void FindShortestPathFromStartToEndBFS_GoalIsReachable()
         {
             var numbersArray = new NumbersArray { Numbers = new List<int> { 1, 2, 0, 3, 0, 2, 0 } };
 
@@ -18,6 +18,67 @@ namespace CodingTaskArrays.Tests
             var expected = new List<int>() { 0, 1, 3, 6 };
 
             Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void FindShortestPathFromStartToEndBFS_GoalIsNotReachableEmptyNumbersArray()
+        {
+            var numbersArray = new NumbersArray { Numbers = new List<int> { } };
+
+            var result = ShortestPathFinder.FindShortestPathFromStartToEndBFS(GraphBuilder.Build(numbersArray));
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void FindShortestPathFromStartToEndBFS_GoalIsReachableAllOnes()
+        {
+            var numbersArray = new NumbersArray { Numbers = new List<int> { 1, 1, 1, 1 } };
+
+            var result = ShortestPathFinder.FindShortestPathFromStartToEndBFS(GraphBuilder.Build(numbersArray));
+            var expected = new List<int>() { 0, 1, 2, 3 };
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void FindShortestPathFromStartToEndBFS_GoalIsNotReachableAllZeros()
+        {
+            var numbersArray = new NumbersArray { Numbers = new List<int> { 0, 0, 0, 0 } };
+
+            var result = ShortestPathFinder.FindShortestPathFromStartToEndBFS(GraphBuilder.Build(numbersArray));
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void FindShortestPathFromStartToEndBFS_GoalIsNotReachable2()
+        {
+            var numbersArray = new NumbersArray { Numbers = new List<int> { 1, 2, 0, 1, 0, 2, 0 } };
+
+            var result = ShortestPathFinder.FindShortestPathFromStartToEndBFS(GraphBuilder.Build(numbersArray));
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void FindShortestPathFromStartToEndBFS_GoalIsNotReachableNegativeNumber1()
+        {
+            var numbersArray = new NumbersArray { Numbers = new List<int> { 1, 2, 1, -1, 0, 2, 0 } };
+
+            var result = ShortestPathFinder.FindShortestPathFromStartToEndBFS(GraphBuilder.Build(numbersArray));
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void FindShortestPathFromStartToEndBFS_GoalIsNotReachableNegativeNumber2()
+        {
+            var numbersArray = new NumbersArray { Numbers = new List<int> { 1, 2, 0, -1, 0, 2, 0 } };
+
+            var result = ShortestPathFinder.FindShortestPathFromStartToEndBFS(GraphBuilder.Build(numbersArray));
+
+            Assert.Null(result);
         }
     }
 }
